48bb1ff963ea0268781dc63d91cf971fcbd0ca6c
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-03-18 12:19:45 -0400

    initial

---
+++ Arithmatic Operators/src/lesson1/MyClass.java
@@ -0,0 +1,22 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args){
+	double x, y, sum, sub, multi, division, reminder;
+	x = 70;
+	y = 30;
+	sum = x + y;
+	sub = x - y;
+	multi = x * y;
+	division = x / y;
+	reminder = x % y;
+	
+	System.out.println("Summation = " + sum);
+	System.out.println("Subtraction = " + sub);
+	System.out.println("Multiplication = " + multi);
+	System.out.println("Division = " + division);
+	System.out.println("Reminder = " + reminder);
+	
+}
+}
---
+++ Array/src/lesson1/MyClass.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] array={1,2,3,4,5};
+		 System.out.println(array[2]);
+		 
+		 
+		 System.out.println("--------");
+		 
+		 int[] array1= {23,45,71,67};
+		 
+		 int index=0;
+		 while(index< 4){
+			 System.out.println(array1[index]);
+			 index++;
+		 }
+		 
+		 
+
+	}
+
+}
