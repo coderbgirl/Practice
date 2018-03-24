2edfc3df83a0021e3a981b4208f42561c5c63805
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-03-24 15:51:36 -0400

    loop

---
+++ For Loop/src/lesson1/Class2.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a={1,2,3,4,5};
+		for(int element:a)
+		{
+			System.out.println(element);
+		}
+
+	}
+
+}
---
+++ For Loop/src/lesson1/MyClass.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a= {1,2,3,4,5};
+		int i;
+		
+		for(i=0; i<5;i++)
+		{
+			System.out.println(a[i]);
+			
+		}
+
+		
+	}
+
+}
