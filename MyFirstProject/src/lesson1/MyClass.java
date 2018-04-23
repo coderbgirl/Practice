fd307bc79c4686125e5d183010660f3a42be913a
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-04-22 22:42:44 -0400

    initial

---
+++ MyFirstProject/src/lesson1/MyClass.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Youtube");
+		String name= "abc";
+		System.out.printf("%4s",name);
+	
+
+	}
+}
+
+
+
