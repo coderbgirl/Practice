259aeec7ba3e143d143728de8e54e8292d248c3c
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-04-28 18:16:38 -0400

    initial

---
+++ Public Private Protected/src/lesson1/MyClass.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student stu= new Student();
+		stu.setName("Mark");
+		System.out.println(stu.getName());
+		
+
+	}
+
+}
