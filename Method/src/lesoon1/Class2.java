6bf3ccf5194f303682537434f3031a6ca1b08f3f
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-04-28 18:16:27 -0400

    initial

---
+++ Method/src/lesoon1/Class2.java
@@ -0,0 +1,27 @@
+package lesoon1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		myMethod();
+		myMethod();
+		myMethod();
+		myMethod();
+
+
+	}
+
+	public static void myMethod(){
+		
+		System.out.println("Hello");
+	}
+	
+
+
+
+
+	
+
+}
