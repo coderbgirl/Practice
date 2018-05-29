638345a9bbb80da3c25b19388bad76cc8c85df60
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-05-29 19:10:03 -0400

    initial

---
+++ String/src/lesson1/MyClass.java
@@ -0,0 +1,37 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "Hello World";
+		System.out.println(a);
+		 
+		int b= a.length();
+		System.out.println(b);
+		
+		String c= a.toLowerCase();
+		System.out.println(c);
+		
+		String d= a.toUpperCase();
+		System.out.println(d);
+		
+		String e= "Hello"+"World";
+		System.out.println(e);
+		
+		System.out.println(a.replace('e','p'));
+		
+		System.out.println(a.indexOf('W'));
+		
+		 
+		 
+		 
+		 
+		 
+		
+		
+
+	}
+
+}
